76bbb936364782e4141ead022435d10a5d6e120f
Aaron Stewart <astewart.john@gmail.com>
2014-06-09 21:09:07 -0600

    first commit, although it's almost done.

---
+++ js/site.js
@@ -0,0 +1,12 @@
+    // ----------- Number Counter--------------
+    // ----------------------------------------
+    $({numberCount: $('#counter').text()}).animate({numberCount: 10000}, 
+      {
+        duration: 3500,
+        step: function() {
+          $('#counter').text(Math.floor(this.numberCount));
+        },
+        complete: function() {
+          $('#counter').text("10,000");
+        }
+      });
